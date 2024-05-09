a011d4899cd2f36d95e0782ef66adc2dbfa4966e
Dhaval Khandla <dhavaljk@ti.com>
2024-05-09 20:51:52 +0530

    am64x/am243x: endat: Fix firmware build warnings
    - Update the top level project.js file to sort the entries in
      component_file_list and example_file_list
    
    Fixes: PINDSW-7783
    
    Signed-off-by: Dhaval Khandla <dhavaljk@ti.com>

--- source/position_sense/endat/firmware/multi_channel_load_share/.project/project_am243x.js
+++ source/position_sense/endat/firmware/multi_channel_load_share/.project/project_am243x.js
@@ -60,6 +60,7 @@ const cflags = {
 
 const lflags = {
     common: [
+        "--diag_suppress=10063-D", /* Added to suppress entry_point related warning */
         "--entry_point=ENDAT_INIT",
         "--disable_auto_rts",
     ],
