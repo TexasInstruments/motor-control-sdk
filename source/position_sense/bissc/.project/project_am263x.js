95d4b72c463dd28a9b8a3df4422386d7f24f35f4
Arnav Raj <a-raj3@ti.com>
2025-01-02 14:46:24 +0530

    am263x: bissc: Add example, driver and firmware
    - Add single channel example
    
    Fixes: PINDSW-8168
    
    Signed-off-by: Arnav Raj <a-raj3@ti.com>

---
+++ source/position_sense/bissc/.project/project_am263x.js
@@ -0,0 +1,53 @@
+let path = require('path');
+
+let device = "am263x";
+
+const files = {
+    common: [
+        "bissc_drv.c",
+    ],
+};
+
+const filedirs = {
+    common: [
+        "${MOTOR_CONTROL_SDK_PATH}/source/position_sense/bissc/driver",
+    ],
+};
+
+const includes = {
+    common: [
+        "${MOTOR_CONTROL_SDK_PATH}/source/position_sense/bissc/include",
+        "${MOTOR_CONTROL_SDK_PATH}",
+    ],
+};
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5f", cgt: "ti-arm-clang"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "library";
+    property.name = "motorcontrol_bissc";
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.includes = includes;
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
