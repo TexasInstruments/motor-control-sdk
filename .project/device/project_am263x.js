5f5c97a20edc06893873f09832680593d2d73068
Dhaval Khandla <dhavaljk@ti.com>
2025-05-21 16:56:34 +0530

    am263x: bissc: Add example, driver and firmware
    - Add single channel example
    
    Fixes: PINDSW-8168
    
    Signed-off-by: Arnav Raj <a-raj3@ti.com>

--- .project/device/project_am263x.js
+++ .project/device/project_am263x.js
@@ -1,6 +1,7 @@
 const common = require("../common.js");
 
 const component_file_list = [
+    "source/position_sense/bissc/.project/project.js",
     "source/position_sense/endat/.project/project.js",
     "source/position_sense/nikon/.project/project.js",
     "source/position_sense/tamagawa_over_soc_uart/.project/project.js",
@@ -16,6 +17,7 @@ const device_defines = {
 const example_file_list = [
     "examples/dcl/dcl_df22/.project/mcsdk_project.js",
     "examples/dcl/dcl_pi/.project/mcsdk_project.js",
+    "examples/position_sense/bissc_diagnostic/single_channel/.project/project.js",
     "examples/position_sense/endat_diagnostic/single_channel/.project/project.js",
     "examples/position_sense/nikon_diagnostic/single_channel/.project/project.js",
     "examples/position_sense/tamagawa_diagnostic_over_soc_uart/.project/project.js",
@@ -23,6 +25,7 @@ const example_file_list = [
     "examples/sfra/.project/mcsdk_project.js",
     "examples/transforms/transforms_test/.project/mcsdk_project.js",
     "examples/utilities/datalog/.project/mcsdk_project.js",
+    "source/position_sense/bissc/firmware/single_channel/.project/project.js",
     "source/position_sense/endat/firmware/single_channel/.project/project.js",
     "source/position_sense/nikon/firmware/single_channel/.project/project.js",
     "source/position_sense/tamagawa/firmware/single_channel/.project/project.js",
