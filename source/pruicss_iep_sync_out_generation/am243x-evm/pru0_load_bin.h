2b115f5cdb52484fe8d7d08ebd81a8cb8bc20f8e
Manoj Koppolu <manoj_koppolu@ti.com>
2025-09-08 09:27:58 +0530

    am243x: timesync: add time sync example
    Fixes: PINDSW-9415
    
    Signed-off-by: Manoj Koppolu <manoj_koppolu@ti.com>

---
+++ source/pruicss_iep_sync_out_generation/am243x-evm/pru0_load_bin.h
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (C) 2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const uint32_t PRU0_Firmware_0[] = {
+0x2effbb80,
+0x32000000,
+0x240002c6,
+0x24a00086,
+0x240ffee7,
+0xe1000687,
+0x2efe0060,
+0x240002c6,
+0x24a00086,
+0x240000e7,
+0xe1000687,
+0x240002c6,
+0x24a00086,
+0x240ffee7,
+0xe1002687,
+0x240002c6,
+0x24a00086,
+0x24003de7,
+0x24004ae8,
+0x24004ae9,
+0xe108a687,
+0x24004ae7,
+0x24004ae8,
+0xe1146687,
+0x240002c6,
+0x24a00086,
+0xf1002687,
+0x1d03e7e7,
+0xe1002687,
+0x240002c6,
+0x24a00086,
+0xf1002687,
+0x1d04e7e7,
+0xe1002687,
+0x240002c6,
+0x24a00086,
+0xf1002687,
+0x1d05e7e7,
+0xe1002687,
+0x240002c6,
+0x24a00086,
+0xf1002687,
+0x1d06e7e7,
+0xe1002687,
+0x240002c6,
+0x24a00086,
+0x2eff8387,
+0x13110707,
+0x13002727,
+0x13004747,
+0x13006767,
+0x13000808,
+0xe1384607,
+0x32800000,
+0x240001c3,
+0x2486a083,
+0x240001c5,
+0x2486a085,
+0x00e5e3e3,
+0x0300e4e4,
+0x79000000,
+0x2400c8e2,
+0x30e20002,
+0x10000000,
+0x240000e2,
+0x81800882,
+0x81807a83,
+0x240003e2,
+0x81800882,
+0x00e5e3e3,
+0x0300e4e4,
+0x2efe007d,
+0x10000000,
+0x10000000,
+0x2efe007d,
+0x10000000,
+0x10000000};
+
