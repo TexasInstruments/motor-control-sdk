72123da4e42822fc4db28ae076a595e9dad98e9d
Dhaval Khandla <dhavaljk@ti.com>
2026-01-28 16:05:35 +0530

    allsoc: infra: Update license based on updated templates
    Fixes: PINDSW-10147
    
    Signed-off-by: Dhaval Khandla <dhavaljk@ti.com>

--- source/position_sense/endat3/firmware/single_channel/endat3_receiver_pru1_bin.h
+++ source/position_sense/endat3/firmware/single_channel/endat3_receiver_pru1_bin.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Texas Instruments Incorporated
+ * Copyright (C) 2025-2026 Texas Instruments Incorporated
  * All rights reserved not granted herein.
  *
  * Limited License.
