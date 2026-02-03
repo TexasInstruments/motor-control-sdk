0d95d1757aaa04cd32b8389721b9cfd349a3dfd3
Achala Ram <a-ram@ti.com>
2026-02-03 18:03:11 +0530

    am243x: sdfm: Update Example Names
    - rename continuous mode examples to include "load share" and "single PRU" in the name.
    - rename "three channel single pru" example to "nine channel single pru".
    
    Fixes: PINDSW-9661
    
    Signed-off-by: Achala Ram a-ram@ti.com

--- .project/device/project_am243x.js
+++ .project/device/project_am243x.js
@@ -18,11 +18,11 @@ const device_defines = {
 };
 
 const example_file_list = [
+    "examples/current_sense/icss_sdfm_nine_channel_load_share_continuous_mode/.project/project.js",
     "examples/current_sense/icss_sdfm_nine_channel_load_share_snoop_mode/.project/project.js",
-    "examples/current_sense/icss_sdfm_nine_channel_with_continuous_mode/.project/project.js",
-    "examples/current_sense/icss_sdfm_three_channel_single_pru_mode/.project/project.js",
+    "examples/current_sense/icss_sdfm_nine_channel_single_pru_mode/.project/project.js",
+    "examples/current_sense/icss_sdfm_three_channel_single_pru_continuous_mode/.project/project.js",
     "examples/current_sense/icss_sdfm_three_channel_single_pru_snoop_mode/.project/project.js",
-    "examples/current_sense/icss_sdfm_three_channel_with_continuous_mode/.project/project.js",
     "examples/current_sense/icss_sdfm_three_channel_with_phase_compensation/.project/project.js",
     "examples/position_sense/bissc_diagnostic/multi_channel_load_share/.project/project.js",
     "examples/position_sense/bissc_diagnostic/multi_channel_single_pru/.project/project.js",
