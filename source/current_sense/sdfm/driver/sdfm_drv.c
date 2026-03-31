9cbc0ed896e81081fe6d4c6ea01c9a1fb885384b
Dhaval Khandla <dhavaljk@ti.com>
2026-03-31 15:29:04 +0530

    am243x: sdfm: Fix SDFM_selectIepCmpEvent function
    - Use correct base address for IEP1
    
    Fixes: PINDSW-10436
    
    Signed-off-by: Dhaval Khandla <dhavaljk@ti.com>

--- source/current_sense/sdfm/driver/sdfm_drv.c
+++ source/current_sense/sdfm/driver/sdfm_drv.c
@@ -1756,7 +1756,7 @@ int32_t SDFM_selectIepCmpEvent(SDFM_Handle handle, uint8_t event, uint8_t pru_co
     else if(attrs->iep_instance == PRUICSS_IEP_INST1)
     {
         /* Enable IEP1 as trigger source */
-        pru_iep = (void *)(((PRUICSS_HwAttrs *)(priv->pruicss_handle->hwAttrs))->iep0RegBase);
+        pru_iep = (void *)(((PRUICSS_HwAttrs *)(priv->pruicss_handle->hwAttrs))->iep1RegBase);
         iep_cmp_reg = CSL_ICSS_G_PR1_IEP1_SLV_REGS_BASE + CSL_ICSS_G_PR1_IEP0_SLV_CMP0_REG0 + (event * 8);
         iep_cmp_status_reg = CSL_ICSS_G_PR1_IEP1_SLV_REGS_BASE + CSL_ICSS_G_PR1_IEP0_SLV_CMP_STATUS_REG;
     }
